51efc543347bb7b82c25c449a6afd63ef95d11f7
LemaoSquared <tagleallan321@gmail.com>
2026-03-11 09:21:22 +0800

    Revert "Core Gameplay Update: Better Gameplay Experience"

--- Scripts/square.gd
+++ Scripts/square.gd
@@ -1,16 +1,8 @@
 extends Node2D
 
 @export var type_name = "Square"
-var speed := 0
-var acceleration := 0
-var decceleration := 0
-var is_clashed := false
 
-func setup(spd, acc, dec):
-	speed = spd
-	acceleration = acc
-	decceleration = dec
-	
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
